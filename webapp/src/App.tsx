38eba152963db9bd52bc81ece2a4381d7584c5a4
Alex Ventura <alex.rv11@gmail.com>
2022-11-06 13:53:27 -0300

    add theme provider and header

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Stack from '@mui/material/Stack';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-function App() {
+import './App.css';
+import Header from './components/Header';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
+
+const App = () => {
+  const connect = () => {
+    console.log('connect wallet');
+  }
+
+  const disconnect = () => {
+    console.log('disconnect wallet');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack spacing={2} sx={{ flexGrow: 1 }}>
+      <ThemeProvider theme={darkTheme}>
+        <div className="App">
+          <Header walletConnected={true} balance={"10 ETH"} address={"address-test"} connect={connect} disconnect={disconnect}></Header>
+        </div>
+      </ThemeProvider>
+    </Stack>
   );
 }
 
